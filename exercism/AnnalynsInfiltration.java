2503113b57cb4ba17413f0b4b3ace9b52a99cff7
Layla Barbosa <122912928+laylabarbosa@users.noreply.github.com>
2023-03-12 12:45:34 -0400

    Create AnnalynsInfiltration.java
    commit add exercise 3

---
+++ exercism/AnnalynsInfiltration.java
@@ -0,0 +1,19 @@
+class AnnalynsInfiltration {
+    
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return prisonerIsAwake || knightIsAwake || archerIsAwake;
+    }
+    
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {  
+        return !archerIsAwake && !knightIsAwake && prisonerIsAwake || (petDogIsPresent && !archerIsAwake);
+        }
+}
